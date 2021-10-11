4ecce3fa7d9778fc0c4d2d9092028b99f24274d0
sunlordc1 <sunlordc1@gmail.com>
2021-10-11 10:43:07 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var ultils = require('./ultils');
+var chalk = require('chalk')
+//G
+console.log(ultils)
+////////////////////////////////////////////////////////////////////
 var fs = require('fs')
 // var ketqua = add(8,9);
 //Đây là hàm đọc dữ liệu từ 1 file nào đó 1 cách bất đồng bộ
 fs.readFile('./dulieu.txt',function(err,data){
     console.log(data.toString());
 })
-console.log(ultils)
+
+////////////////////////////////////////////////////////////////////
+var path  = require('path');
+var duongDan = path.join('2: '+__dirname+ '\\index.js');
+console.log('1: '+__dirname + '\\index.js');
+
+console.log(chalk.blue(duongDan))
+
+
+
+
